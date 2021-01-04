154bcceaf02c17b2b0bc77773a0bb2cfa1a79a92
Sean LaFlam <sean.laflam@gmail.com>
2021-01-04 14:28:24 -0500

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Characters from './containers/Characters';
+import Team from './containers/Team';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    team: []
+  }
+
+  clickHandler = (character) => {
+    // figure out how to pass that character to Team
+    let copiedArray = [character, ...this.state.team]
+    this.setState(() => ({ team: copiedArray }))
+  }
+
+  loginHandler = (userInfo) => {
+    console.log("logging in", userInfo)
+    fetch("https://localhost:3000/api/v1/login", {
+      method: "POST",
+      headers: {
+        accepts: "application/json",
+        "content-type": "application/json"
+      },
+      body: JSON.stringify({user: userInfo})
+    })
+    .then(r => r.json())
+    .then(data => {
+      console.log("token:", data.token)
+    })
+  }
+  render() {
+
+    return (
+      <div className="App">
+        <Team characters={this.state.team} />
+        <Characters clickHandler={this.clickHandler} />
+      </div>
+    );
+  }
 }
 
 export default App;
